58bca7787163266d4a94c24f3cbc51f74b23de2d
Charbel Azar <77696672+Charbel-Azar@users.noreply.github.com>
2025-02-15 03:19:50 +0200

    asd

--- assests/js/camera.js
+++ assests/js/camera.js
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function() {
       window.scrollTo(0, initialScroll);
       video.pause();
     }, 1000);
-  }, 1000);
+  }, 2000);
 
   function triggerFlash() {
     flash.style.opacity = '1';
